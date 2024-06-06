a340c7ba1b0f6325d49dfd8e2c92c328df70bfb4
kevitruon <kevitruon@gmail.com>
2024-06-06 15:36:39 -0700

    Clean up code

--- kevitruon/src/App.tsx
+++ kevitruon/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
 import Portfolio from './components/Portfolio';
@@ -12,7 +11,6 @@ function App() {
   return (
     <div>
       <Navbar />
-      <Hero />
       <About />
       <Skills />
       <Portfolio />
