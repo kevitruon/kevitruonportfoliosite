126f30c9e89114eea6fc84857e521094b013f81a
kevitruon <kevitruon@gmail.com>
2024-06-18 15:45:18 -0700

    Cleanup

--- kevitruon/src/App.tsx
+++ kevitruon/src/App.tsx
@@ -6,11 +6,14 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div>
+
       <Navbar />
+      <Hero />
       <About />
       <Skills />
       <Portfolio />
--- kevitruon/src/components/About.tsx
+++ kevitruon/src/components/About.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const About: React.FC = () => {
   return (
@@ -10,16 +10,22 @@ const About: React.FC = () => {
           <div className="mb-8">
             <h3 className="text-xl font-semibold mb-2">2023-2024</h3>
             <p className="text-gray-700 mb-4">
-              <strong>Hack Reactor (Galvanize) - Advanced Software Engineering Certificate</strong>
+              <strong>
+                Hack Reactor (Galvanize) - Advanced Software Engineering
+                Certificate
+              </strong>
             </p>
             <p className="text-gray-600">
-              To further hone my skills and prepare for a career in software engineering, I recently graduated from
-              Hack Reactor's Advanced Coding Bootcamp. This program provided me with real-world training in Full-
-              Stack web development using cutting-edge technologies and frameworks. Through projects and immersive
-              learning experiences, I became proficient in tools like React, Redux, Express, PostgreSQL, and modern
-              development workflows. Hack Reactor's intensive program allowed me to translate my theoretical
-              knowledge into practical real-world applications, enabling me to hit the ground running and deliver user-
-              friendly software solutions.
+              To further hone my skills and prepare for a career in software
+              engineering, I recently graduated from Hack Reactor's Advanced
+              Coding Bootcamp. This program provided me with real-world training
+              in Full-Stack web development using cutting-edge technologies and
+              frameworks. Through projects and immersive learning experiences, I
+              became proficient in tools like React, Redux, Express, PostgreSQL,
+              and modern development workflows. Hack Reactor's intensive program
+              allowed me to translate my theoretical knowledge into practical
+              real-world applications, enabling me to hit the ground running and
+              deliver user-friendly software solutions.
             </p>
           </div>
           <div>
@@ -28,19 +34,16 @@ const About: React.FC = () => {
               <strong>UC Santa Cruz - Computer Science: Game Design</strong>
             </p>
             <p className="text-gray-600">
-              My academic journey began at UC Santa Cruz, where
-              I immersed myself in a wide range of computer science
-              courses. Though I had to separate from the
-              university due to personal circumstances,
-              I completed 134 credits and earned invaluable
-              knowledge through classes such as
-              Programming Abstraction, Computer Graphics,
-              Data Structures &amp; Algorithms, and Embedded/Systems
-              Programming. These courses instilled in me a strong
-              foundation in core concepts, algorithmic thinking,
-              data organization, computer graphics, and low-level
-              systems programming, igniting my passion for creating
-              innovative digital experiences.
+              My academic journey began at UC Santa Cruz, where I immersed
+              myself in a wide range of computer science courses. Though I had
+              to separate from the university due to personal circumstances, I
+              completed 134 credits and earned invaluable knowledge through
+              classes such as Programming Abstraction, Computer Graphics, Data
+              Structures &amp; Algorithms, and Embedded/Systems Programming.
+              These courses instilled in me a strong foundation in core
+              concepts, algorithmic thinking, data organization, computer
+              graphics, and low-level systems programming, igniting my passion
+              for creating innovative digital experiences.
             </p>
           </div>
         </div>
