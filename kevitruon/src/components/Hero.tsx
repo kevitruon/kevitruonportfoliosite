b4fa65716c5bbcf8a8979c33ff962fd35b63763e
kevitruon <146457163+kevitruon@users.noreply.github.com>
2024-07-02 00:01:26 -0700

    Update Hero.tsx

--- kevitruon/src/components/Hero.tsx
+++ kevitruon/src/components/Hero.tsx
@@ -2,6 +2,29 @@
 import React from "react";
 
 const Hero: React.FC = () => {
-  return (<div id="Home">Hero</div>);
+  return (
+    <div className="max-w-4xl mx-auto py-16 px-4 sm:px-6 lg:px-8" id="About">
+      <h1 className="text-4xl font-bold mb-8">Welcome to my portfolio!
+      </h1>
+      <div>
+        <div>
+            <p className="">
+              To further hone my skills and prepare for a career in software
+              engineering, I recently graduated from Hack Reactor's Advanced
+              Coding Bootcamp. This program provided me with real-world training
+              in Full-Stack web development using cutting-edge technologies and
+              frameworks. Through projects and immersive learning experiences, I
+              became proficient in tools like React, Redux, Express, PostgreSQL,
+              and modern development workflows. Hack Reactor's intensive program
+              allowed me to translate my theoretical knowledge into practical
+              real-world applications, enabling me to hit the ground running and
+              deliver user-friendly software solutions.
+            </p>
+            <img src="" alt="" />
+          </div>
+          </div>
+          </div>
+
+);
 };
 export default Hero;
